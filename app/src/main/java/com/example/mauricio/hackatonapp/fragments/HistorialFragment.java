df68fe6cf71e4a5b490c388c4a3650f2714a830b
DiegoVz74 <diego_val2008@hotmail.com>
2018-04-05 06:22:04 -0500

    Framents

---
+++ app/src/main/java/com/example/mauricio/hackatonapp/fragments/HistorialFragment.java
@@ -0,0 +1,36 @@
+package com.example.mauricio.hackatonapp.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mauricio.hackatonapp.R;
+
+
+public class HistorialFragment extends Fragment {
+
+
+    public HistorialFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_historial, container, false);
+    }
+
+}
